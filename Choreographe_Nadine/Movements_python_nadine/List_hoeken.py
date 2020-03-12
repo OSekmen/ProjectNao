c580bd195ff9587cff1378c709a03bc17da9448c
Nadine0508 <38976791+Nadine0508@users.noreply.github.com>
2020-03-12 12:03:00 +0100

    Nadine Movement

---
+++ Choreographe_Nadine/Movements_python_nadine/List_hoeken.py
@@ -0,0 +1,8 @@
+class data(object):
+    def _init_ (self,ahoeken):
+        super(Hoeken,self)._init_()
+        self.hoeken = ahoeken
+    def hoek (self,message):
+        self.logger(message)
+        AllIN = []
+        AllIN.append [1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
